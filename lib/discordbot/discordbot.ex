7e6eb6874ffebbeacc15cb19eeb88ac832118749
Victor Torres <victor.tmeloo@gmail.com>
2022-04-13 03:25:27 -0300

    Adding more APIs

--- lib/discordbot/discordbot.ex
+++ lib/discordbot/discordbot.ex
@@ -13,20 +13,85 @@ defmodule Discordbot.Consumer do
 
         # COMANDOS
 
+        # API Valorant
+
+        msg.content == "!valorant" -> valorant(msg)
+
+        #API Words
+
+        String.starts_with?(msg.content, "!words ") -> wordsDefinition(msg)
+
+        msg.content == "!words" -> Api.create_message(msg.channel_id, "Use **!words <palavra>** para descobir o significado da palavra.")
+
+        #API tabela serie A
+
+        msg.content == "!brasileirao" -> brasileirao(msg)
+
+        # API preço de jogos
+
         String.starts_with?(msg.content, "!gameprice ") -> gamePrice(msg)
 
         msg.content == "!gameprice" -> Api.create_message(msg.channel_id, "Use **!gameprice <nome do jogo>** para descobir o preço do jogo na Steam.")
 
-        # Caso Geral
-        String.starts_with?(msg.content, "!") -> Api.create_message(msg.channel_id, "Comando inválido, tente novamente.")
-
         # Listar comandos existentes
         msg.content == "!comandos" -> Api.create_message(msg.channel_id, "Os comandos existentes no bot são: **!gameprice**, **...**")
+
+        # Caso Geral
+        String.starts_with?(msg.content, "!") -> Api.create_message(msg.channel_id, "Comando inválido, tente novamente.")
 
         # Nada
         true -> :ignore
 
       end
+
+    end
+
+    defp valorant(msg) do
+
+      response = HTTPoison.get!("https://valorant-agents-maps-arsenal.p.rapidapi.com/agents/pt-br", [{"X-RapidAPI-Host", "valorant-agents-maps-arsenal.p.rapidapi.com"} , {"X-RapidAPI-Key", "125e6ae7ebmsh36882299b848d27p150e1ajsnd54c607719e4"}])
+
+      {:ok ,values} = Poison.decode(response.body)
+
+      agentes = values["agents"]
+
+      Enum.each(agentes,
+       fn x ->
+
+        Api.create_message(msg.channel_id, agentes["title"])
+
+        end
+        )
+
+    end
+
+    defp wordsDefinition(msg) do
+
+      # Quebrando em comando/parametros
+      aux = String.split(msg.content, " ", parts: 2)
+
+      word = Enum.fetch!(aux,1)
+
+      response = HTTPoison.get!("https://wordsapiv1.p.rapidapi.com/words/#{word}/definitions", [{"X-RapidAPI-Host", "wordsapiv1.p.rapidapi.com"} , {"X-RapidAPI-Key", "125e6ae7ebmsh36882299b848d27p150e1ajsnd54c607719e4"}])
+
+      {:ok ,values} = Poison.decode(response.body)
+
+      definition = values["definitions"]["definition"]
+
+      Api.create_message(msg.channel_id, "Definition of #{word}: #{definition}")
+
+      IO.puts(values)
+
+    end
+
+    defp brasileirao(msg) do
+
+      apiKey = "Bearer test_f7f9ad174fbd9cc5fe15e00edc0efb"
+
+      response = HTTPoison.get!("https://api.api-futebol.com.br/v1/campeonatos/10/fases/168", [{"Authorization", apiKey}])
+
+      {:ok ,values} = Poison.decode(response.body)
+
+      IO.puts(values)
 
     end
 
